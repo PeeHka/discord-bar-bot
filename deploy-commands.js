0ae142d5c974d1596434d088b5324c5335847013
PeeHka <kostryuk10@gmail.com>
2026-01-24 17:15:03 +0300

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,7 +3,7 @@ const { TOKEN, CLIENT_ID, GUILD_ID } = process.env;
 
 const commands = [
   new SlashCommandBuilder().setName("help").setDescription("Помощь"),
-  new SlashCommandBuilder().setName("баланс").setDescription("Твой баланс"),
+  new SlashCommandBuilder().setName("баланс").setDescription("Баланс"),
   new SlashCommandBuilder().setName("выпить").setDescription("Выпить"),
   new SlashCommandBuilder().setName("казино").setDescription("Казино"),
   new SlashCommandBuilder().setName("кости").setDescription("Кости"),
@@ -13,36 +13,45 @@ const commands = [
     .setDescription("Купить напиток")
     .addStringOption(o =>
       o.setName("напиток")
-        .setDescription("Что купить")
         .setRequired(true)
         .addChoices(
-          { name: "🍺 пиво", value: "пиво" },
-          { name: "🥃 виски", value: "виски" },
-          { name: "🍾 водка", value: "водка" },
-          { name: "☠ самогон", value: "самогон" },
-          { name: "🧪 абсент", value: "абсент" }
+          { name: "пиво", value: "пиво" },
+          { name: "виски", value: "виски" },
+          { name: "водка", value: "водка" },
+          { name: "самогон", value: "самогон" },
+          { name: "абсент", value: "абсент" }
         )
     ),
-  new SlashCommandBuilder().setName("титул").setDescription("Твой титул"),
-  new SlashCommandBuilder().setName("топ").setDescription("Топ алкашей"),
+  new SlashCommandBuilder().setName("титул").setDescription("Титул"),
+  new SlashCommandBuilder().setName("топ").setDescription("Топ"),
 
-  /* OWNER ONLY */
+  // OWNER ONLY
+  new SlashCommandBuilder()
+    .setName("admin_add")
+    .setDescription("Добавить админа (овнер)")
+    .addUserOption(o => o.setName("пользователь").setRequired(true)),
+
+  new SlashCommandBuilder()
+    .setName("admin_delete")
+    .setDescription("Удалить админа (овнер)")
+    .addUserOption(o => o.setName("пользователь").setRequired(true)),
+
   new SlashCommandBuilder()
     .setName("money_give")
     .setDescription("Выдать валюту (овнер)")
-    .addUserOption(o => o.setName("пользователь").setDescription("Кому").setRequired(true))
-    .addIntegerOption(o => o.setName("количество").setDescription("Сколько").setRequired(true)),
+    .addUserOption(o => o.setName("пользователь").setRequired(true))
+    .addIntegerOption(o => o.setName("количество").setRequired(true)),
 
   new SlashCommandBuilder()
     .setName("money_take")
     .setDescription("Забрать валюту (овнер)")
-    .addUserOption(o => o.setName("пользователь").setDescription("У кого").setRequired(true))
-    .addIntegerOption(o => o.setName("количество").setDescription("Сколько").setRequired(true)),
+    .addUserOption(o => o.setName("пользователь").setRequired(true))
+    .addIntegerOption(o => o.setName("количество").setRequired(true)),
 
   new SlashCommandBuilder()
     .setName("money_reset")
     .setDescription("Сбросить валюту (овнер)")
-    .addUserOption(o => o.setName("пользователь").setDescription("Кому").setRequired(true))
+    .addUserOption(o => o.setName("пользователь").setRequired(true))
 ].map(c => c.toJSON());
 
 const rest = new REST({ version: "10" }).setToken(TOKEN);
