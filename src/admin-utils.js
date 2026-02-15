a0273d0d669b1455ebc721bd61cf801af85152ce
PeeHka <kostryuk10@gmail.com>
2026-02-15 19:54:59 +0300

    Delete src directory

--- src/admin-utils.js
+++
@@ -1,3 +0,0 @@
-const { BOT_OWNER_ID } = require("./config");
-function isOwner(i){ return i.user.id === BOT_OWNER_ID; }
-module.exports = { isOwner };
--- src/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  TOKEN: process.env.TOKEN,
-  CLIENT_ID: process.env.CLIENT_ID,
-  GUILD_ID: process.env.GUILD_ID,
-  MONGO_URI: process.env.MONGO_URI,
-  BOT_OWNER_ID: process.env.BOT_OWNER_ID
-};
