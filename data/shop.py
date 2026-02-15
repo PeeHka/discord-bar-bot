7458bf22f8a3402abde29d7dc05b1f504b694671
PeeHka <kostryuk10@gmail.com>
2026-02-15 20:12:12 +0300

    Delete data directory

--- data/shop.py
+++
@@ -1,6 +0,0 @@
-BUSINESSES = {
-    "Ларёк": {"price": 5000, "income": 200},
-    "Бар": {"price": 20000, "income": 700},
-    "Паб": {"price": 100000, "income": 3000},
-    "Клуб": {"price": 500000, "income": 15000}
-}
