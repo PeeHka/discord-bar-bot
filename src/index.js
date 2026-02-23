297bf29321afccbf64fd0e141012ba33d799087c
PeeHka <kostryuk10@gmail.com>
2026-02-23 14:13:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -84,7 +84,7 @@ client.on("messageCreate", async message => {
 
   try {
     const response = await openai.chat.completions.create({
-      model: "llama3-8b-8192",
+      model: "llama3-70b-8192",
       messages: convo.messages
     });
 
